d64983e64982d9d76c2ea6dd5ba028b3cdea12da
Luis <luisarnaldopalma@gmail.com>
2026-05-07 22:21:08 +0200

    Versión 0. Primer commit del proyecto

---
+++ src/main/java/ed/ud4ejemplogit/UD4EjemploGit.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ed.ud4ejemplogit;
+
+/**
+ * Descripción de la clase
+ */
+public class UD4EjemploGit {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World! desde GIT");
+        System.out.println("Hello World! desde GIT");
+        System.out.println("Hello World! desde GIT");        
+    }
+}
